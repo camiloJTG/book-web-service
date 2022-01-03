b1c94e23c4a10fad3c684ae3c6ffa9ce6acfeefe
camiloJTG <camilojtg.devep@protonmail.com>
2022-01-03 01:07:43 -0300

    feat: Add error middleware

--- src/configs/server.ts
+++ src/configs/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
-
+import { join } from 'path';
 import routes from '../controllers/routes';
 import * as handlerError from '../controllers/middlewares/handlerError';
 
@@ -14,6 +14,8 @@ app.use(helmet());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(join(__dirname, 'public')));
 
 routes(app);
 
